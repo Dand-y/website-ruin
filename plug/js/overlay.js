8d3d3d8754974ecdb28e1e7ef6de39a97515392d
Dand-y <60514024+Dand-y@users.noreply.github.com>
2020-01-31 16:03:51 +0100

    Add files via upload

---
+++ plug/js/overlay.js
@@ -0,0 +1,8 @@
+$('#toggle').click(function() {
+    $(this).toggleClass('toggle-active');
+    $('#overlay').toggleClass('nav-active');
+});
+$('#toggle').click(function() {
+    $(this).hide('toggle-active');
+    $('#overlay').toggleClass('nav-active');
+});
